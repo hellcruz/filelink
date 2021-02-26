0e403c458fbcfdd0bbf928af830dde7ac943815b
hellcruz <69184524+hellcruz@users.noreply.github.com>
2021-02-26 16:37:45 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,19 @@
 import setuptools
-
 from tgfilestream import __version__
-
 try:
     long_desc = open("README.md").read()
 except IOError:
     long_desc = "Failed to read README.md"
-
 setuptools.setup(
     name="tgfilestream",
     version=__version__,
     url="https://mau.dev/tulir/tgfilestream",
-
     author="Tulir Asokan",
     author_email="tulir@maunium.net",
-
     description="A Telegram bot that can stream Telegram files to users over HTTP.",
     long_description=long_desc,
     long_description_content_type="text/markdown",
-
     packages=setuptools.find_packages(),
-
     install_requires=[
         "aiohttp>=3",
         "telethon>=1.10",
@@ -30,7 +23,6 @@ setuptools.setup(
         "fast": ["cryptg>=0.2"],
     },
     python_requires="~=3.7",
-
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
