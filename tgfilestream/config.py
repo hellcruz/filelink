8b4d6880f1ae833827ed7f2f7bd91358d651fd1e
hellcruz <69184524+hellcruz@users.noreply.github.com>
2021-02-26 16:42:00 +0000

    Update config.py

--- tgfilestream/config.py
+++ tgfilestream/config.py
@@ -1,23 +1,6 @@
-# tgfilestream - A Telegram bot that can stream Telegram files to users over HTTP.
-# Copyright (C) 2019 Tulir Asokan
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import sys
 import os
-
 from yarl import URL
-
 try:
     port = int(os.environ.get("PORT", "8080"))
 except ValueError:
@@ -25,7 +8,6 @@ except ValueError:
 if not 1 <= port <= 65535:
     print("Please make sure the PORT environment variable is an integer between 1 and 65535")
     sys.exit(1)
-
 try:
     api_id = "2898344"
     api_hash = "295d449480357269a6f7d74b83fff771"
@@ -33,7 +15,6 @@ except (KeyError, ValueError):
     print("Please set the TG_API_ID and TG_API_HASH environment variables correctly")
     print("You can get your own API keys at https://my.telegram.org/apps")
     sys.exit(1)
-
 trust_headers = bool(os.environ.get("TRUST_FORWARD_HEADERS"))
 host = "0.0.0.0"
 public_url = URL("http://dfilelink.herokuapp.com/")
