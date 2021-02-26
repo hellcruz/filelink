6b8f7a5b90b759eb68249ee45842460cc8efb922
hellcruz <69184524+hellcruz@users.noreply.github.com>
2021-02-26 17:02:22 +0000

    Update log.py

--- tgfilestream/log.py
+++ tgfilestream/log.py
@@ -5,4 +5,4 @@ if log_config:
 else:
     logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
     logging.getLogger("telethon").setLevel(logging.INFO if debug else logging.ERROR)
-log = logging.getLogger("tgfilestream")
+log = logging.getLogger("filestream")
